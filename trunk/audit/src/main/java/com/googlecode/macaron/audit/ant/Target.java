59f636e6ae000f8955da36afb46f9e5035ea2f18
belaran <belaran@41635ea8-dbcb-11dd-b97a-b9dcd130504c>
2009-06-30 22:24:04 +0000

    Adding an other POJO to modelize the element inside the ant task

---
+++ trunk/audit/src/main/java/com/googlecode/macaron/audit/ant/Target.java
@@ -0,0 +1,27 @@
+/**
+ * BSD-style license; for more info see http://xradar.sourceforge.net/license.html
+ */
+package com.googlecode.macaron.audit.ant;
+
+/**
+ * @author Romain PELISSE <belaran@gmail.com>
+ *
+ */
+public class Target {
+
+	private String name;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
