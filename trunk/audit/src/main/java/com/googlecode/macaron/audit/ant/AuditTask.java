0ea5583c995a0c09487a0632c344b439d1f3b825
belaran <belaran@41635ea8-dbcb-11dd-b97a-b9dcd130504c>
2009-06-30 22:24:32 +0000

    First draft for the AntTask implementation

--- trunk/audit/src/main/java/com/googlecode/macaron/audit/ant/AuditTask.java
+++ trunk/audit/src/main/java/com/googlecode/macaron/audit/ant/AuditTask.java
@@ -5,6 +5,10 @@ package com.googlecode.macaron.audit.ant;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
@@ -13,6 +17,13 @@ import com.googlecode.macaron.audit.Audit;
 import com.googlecode.macaron.audit.ParamsConfig;
 
 /**
+ * 
+ * <code>
+ * 		<audit ignore="URL" output="build/audit.xml">
+ * 				<target name="src/main/file/to/analyse"/>
+ *              <target name="src/other/file/to/analyse"/>
+ * 		</audit>
+ * </code>
  * @author Romain PELISSE <belaran@gmail.com>
  *
  */
@@ -21,10 +32,109 @@ public class AuditTask extends Task {
 	private ParamsConfig config = new ParamsConfig();
 	private Audit audit;
 	
+	private String ignore;
+	private String output;
+	private List<Target> targets;
+	private boolean recursive;
+	private String xslt;
 	/*
 	 * Validation of task attributes 
 	 */
 	private void validate() {
+		convertIgnoreToURL();
+		buildFilesList();
+		this.config.setOutput(new File(output));
+		this.config.setRecurs(recursive);
+		this.config.setXslt(xslt);
+	}
+
+	/**
+	 * @return the ignore
+	 */
+	public String getIgnore() {
+		return ignore;
+	}
+
+	/**
+	 * @param ignore the ignore to set
+	 */
+	public void setIgnore(String ignore) {
+		this.ignore = ignore;
+	}
+
+	/**
+	 * @return the output
+	 */
+	public String getOutput() {
+		return output;
+	}
+
+	/**
+	 * @param output the output to set
+	 */
+	public void setOutput(String output) {
+		this.output = output;
+	}
+
+	/**
+	 * @return the targets
+	 */
+	public List<Target> getTargets() {
+		return targets;
+	}
+
+	/**
+	 * @param targets the targets to set
+	 */
+	public void setTargets(List<Target> targets) {
+		this.targets = targets;
+	}
+
+	/**
+	 * @return the recursive
+	 */
+	public boolean isRecursive() {
+		return recursive;
+	}
+
+	/**
+	 * @param recursive the recursive to set
+	 */
+	public void setRecursive(boolean recursive) {
+		this.recursive = recursive;
+	}
+
+	/**
+	 * @return the xslt
+	 */
+	public String getXslt() {
+		return xslt;
+	}
+
+	/**
+	 * @param xslt the xslt to set
+	 */
+	public void setXslt(String xslt) {
+		this.xslt = xslt;
+	}
+
+	private void buildFilesList() {
+		List<File> files = new ArrayList<File>(this.targets.size());
+		for ( Target target : targets ){
+			// Add exist() & readable() test ?
+			files.add(new File(target.getName()));
+		}
+		config.setNames(files);
+	}
+	
+	private void convertIgnoreToURL() {
+		if ( this.ignore != null && ! "".equals(this.ignore)) {
+			try {
+				this.config.setIgnore(new URL(this.ignore));
+			} catch (MalformedURLException e) {
+				throw new BuildException("'ignore' attribute must be a valid URL.");
+			}
+		}
 		
 	}
 	
